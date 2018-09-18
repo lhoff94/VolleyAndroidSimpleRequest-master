b5b7d9609b8094f6f0ea6dabec8e9642b0f87ab6
Lukas Hoffmann <lhoff@zedat.fu-berlin.de>
2018-09-18 15:46:02 +0200

    use resource strings to build url

--- app/src/main/java/in/eyehunt/volleyandroidsimplerequest/Model/SearchFragmentSucheModel.kt
+++ app/src/main/java/in/eyehunt/volleyandroidsimplerequest/Model/SearchFragmentSucheModel.kt
@@ -5,19 +5,20 @@ import `in`.eyehunt.volleyandroidsimplerequest.R.string.*
 import de.fu_berlin.mi.tierklinik.VolleyRequester
 import org.json.JSONObject
 
+
 class SearchFragmentSucheModel(val view: MainActivity){
     val serverRequestModel: ServerRequest by lazy{ ServerRequest() }
 
     fun getOwnerList(ownerFirstNameString: String, ownerLastNameString: String, function: (String) -> Unit) {
         val queue = VolleyRequester.getInstance(view.applicationContext).requestQueue
-        val url = "https://kis20kapp.vetkis.fu-berlin.de:8084//swp/tierklinik/patientRecord/owner?ownerFirtsName=$ownerFirstNameString&ownerLastName=$ownerLastNameString"
+        val url = view.resources.getString(baseurl) + view.resources.getString(requestOwnerList)+ "?ownerFirtsName=" +ownerFirstNameString + "&ownerLastName=" + ownerLastNameString
         serverRequestModel.disableSSLCertificateChecking()
         serverRequestModel.sendRequestToServer(queue,url,({serverAnswer:String ->
         }))
     }
     fun getPatientList(ownerFirstNameString: String, ownerLastNameString: String, patientString: String,returnResponse: (String)-> Unit) {
         val queue = VolleyRequester.getInstance(view.applicationContext).requestQueue
-        val url = "https://kis20kapp.vetkis.fu-berlin.de:8084//swp/tierklinik/patientRecord/animal/owner?ownerFirtsName=$ownerFirstNameString&ownerLastName=$ownerLastNameString&animalName=$patientString"
+        val url = view.resources.getString(baseurl) + view.resources.getString(requestPatientList)+ "?ownerFirtsName=" + ownerFirstNameString + "?ownerLastName=" + ownerLastNameString + "animalName=" + patientString
         serverRequestModel.disableSSLCertificateChecking()
         serverRequestModel.sendRequestToServer(queue,url,({serverAnswer:String ->
             }))
@@ -25,7 +26,7 @@ class SearchFragmentSucheModel(val view: MainActivity){
 
     fun getCase(caseIdString: String, returnResponse: (Int) -> Unit) {
         val queue = VolleyRequester.getInstance(view.applicationContext).requestQueue
-        val url: String = getString(baseurl) + getString(requestCaseById) + caseIdString
+        val url: String =  view.resources.getString(baseurl) + view.resources.getString(requestCaseById) + caseIdString
         serverRequestModel.disableSSLCertificateChecking()
         serverRequestModel.sendRequestToServer(queue, url,({ serverAnswer: String ->
             val jsonCaseFile: JSONObject = JSONObject(serverAnswer).getJSONArray("content").get(0) as JSONObject
@@ -35,7 +36,7 @@ class SearchFragmentSucheModel(val view: MainActivity){
 
     fun getPatient(patientIdString: String, returnResponse: (Int) -> Unit) {
         val queue = VolleyRequester.getInstance(view.applicationContext).requestQueue
-        val url :string = getString(baseurl) + getString(requestCaseById) + patientIdString
+        val url = view.resources.getString(baseurl) + view.resources.getString(requestCaseById) + patientIdString
         serverRequestModel.disableSSLCertificateChecking()
         serverRequestModel.sendRequestToServer(queue, url,({ serverAnswer: String ->
             val patientID: Int = JSONObject(serverAnswer).getJSONObject("content").get("patientId") as Int
