62066caeed875c05060548859d9fc19424e69a22
Lukas Hoffmann <lhoff@zedat.fu-berlin.de>
2018-09-18 15:24:35 +0200

    servercalls for caseId and PatientId

--- app/src/main/java/in/eyehunt/volleyandroidsimplerequest/MainPresenter.kt
+++ app/src/main/java/in/eyehunt/volleyandroidsimplerequest/MainPresenter.kt
@@ -2,7 +2,6 @@ package `in`.eyehunt.volleyandroidsimplerequest
 
 import `in`.eyehunt.volleyandroidsimplerequest.Model.SearchFragmentSucheModel
 import android.content.Intent
-import de.fu_berlin.mi.tierklinik.VolleyRequester
 
 
 class MainPresenter(var view: MainActivity){
@@ -13,33 +12,44 @@ class MainPresenter(var view: MainActivity){
             patientString: String,
             patientIdString: String,
             caseIdString: String) {
-        val queue = VolleyRequester.getInstance(view.applicationContext).requestQueue
-        val url = "https://kis20kapp.vetkis.fu-berlin.de:8084/swp/tierklinik/patientRecord/owner?ownerFirtsName=Nico"
 
         if (caseIdString.isNotBlank()) {
-            backendData.getCase(caseIdString, ({ response: Int -> receiveCase(response) }))
+            backendData.getCase(caseIdString, ({ response: Int -> recieveCase(response) }))
             return
         }
         if (patientIdString.isNotBlank()) {
-            backendData.getPatient(patientIdString, ({ response: Int -> receivePatient(response) }))
+            backendData.getPatient(patientIdString, ({ response: Int -> recievePatient(response) }))
             return
         }
 
         if (patientString.isNotBlank()){
-            if (ownerFirstNameString.isNotBlank() and ownerLastNameString.isNotBlank()){
-                backendData.getPatientList()
-            }
+            backendData.getPatientList(ownerFirstNameString, ownerLastNameString, patientString,({response: String -> recievePatientList(response)}))
+            return
+        }
+        if (patientString.isBlank()){
+            backendData.getOwnerList(ownerFirstNameString, ownerLastNameString,({response: String -> recieveOwnerList(response)}))
         }
 
+
+
+
+
     }
-    private fun receivePatient(response: Int) {
+
+    private fun recievePatient(response: Int) {
         val intent = Intent(view, MainActivity::class.java)
         intent.putExtra("patient", response)
     }
 
-    private fun receiveCase(response: Int) {
+    private fun recieveCase(response: Int) {
         val intent = Intent(view, MainActivity::class.java)
         intent.putExtra("caseID", response)
+    }
+    private fun recievePatientList(response: String){
+
+    }
+    private fun recieveOwnerList(response:String){
+
     }
 
 
--- app/src/main/java/in/eyehunt/volleyandroidsimplerequest/Model/SearchFragmentSucheModel.kt
+++ app/src/main/java/in/eyehunt/volleyandroidsimplerequest/Model/SearchFragmentSucheModel.kt
@@ -1,33 +1,45 @@
 package `in`.eyehunt.volleyandroidsimplerequest.Model
 
 import `in`.eyehunt.volleyandroidsimplerequest.MainActivity
+import `in`.eyehunt.volleyandroidsimplerequest.R.string.*
 import de.fu_berlin.mi.tierklinik.VolleyRequester
-import org.json.JSONArray
 import org.json.JSONObject
 
 class SearchFragmentSucheModel(val view: MainActivity){
     val serverRequestModel: ServerRequest by lazy{ ServerRequest() }
 
-    fun getOwnerList(){}
-    fun getPatientList(){}
-    fun getPatient(){}
-    fun getCase( caseIdString: String, showResponse: (Int) -> Unit) {
+    fun getOwnerList(ownerFirstNameString: String, ownerLastNameString: String, function: (String) -> Unit) {
         val queue = VolleyRequester.getInstance(view.applicationContext).requestQueue
-        val url = "https://kis20kapp.vetkis.fu-berlin.de:8084/swp/tierklinik/patientRecord/case?caseId=$caseIdString"
+        val url = "https://kis20kapp.vetkis.fu-berlin.de:8084//swp/tierklinik/patientRecord/owner?ownerFirtsName=$ownerFirstNameString&ownerLastName=$ownerLastNameString"
+        serverRequestModel.disableSSLCertificateChecking()
+        serverRequestModel.sendRequestToServer(queue,url,({serverAnswer:String ->
+        }))
+    }
+    fun getPatientList(ownerFirstNameString: String, ownerLastNameString: String, patientString: String,returnResponse: (String)-> Unit) {
+        val queue = VolleyRequester.getInstance(view.applicationContext).requestQueue
+        val url = "https://kis20kapp.vetkis.fu-berlin.de:8084//swp/tierklinik/patientRecord/animal/owner?ownerFirtsName=$ownerFirstNameString&ownerLastName=$ownerLastNameString&animalName=$patientString"
+        serverRequestModel.disableSSLCertificateChecking()
+        serverRequestModel.sendRequestToServer(queue,url,({serverAnswer:String ->
+            }))
+    }
+
+    fun getCase(caseIdString: String, returnResponse: (Int) -> Unit) {
+        val queue = VolleyRequester.getInstance(view.applicationContext).requestQueue
+        val url: String = getString(baseurl) + getString(requestCaseById) + caseIdString
         serverRequestModel.disableSSLCertificateChecking()
         serverRequestModel.sendRequestToServer(queue, url,({ serverAnswer: String ->
             val jsonCaseFile: JSONObject = JSONObject(serverAnswer).getJSONArray("content").get(0) as JSONObject
             val caseID = jsonCaseFile.get("caseId") as Int
-            showResponse(caseID) }) )
+            returnResponse(caseID) }) )
     }
 
-    fun getPatient(patientIdString: String, showResponse: (Int) -> Unit) {
+    fun getPatient(patientIdString: String, returnResponse: (Int) -> Unit) {
         val queue = VolleyRequester.getInstance(view.applicationContext).requestQueue
-        val url = "https://kis20kapp.vetkis.fu-berlin.de:8084/swp/tierklinik/patientRecord//animal/id?animalId=$patientIdString"
+        val url :string = getString(baseurl) + getString(requestCaseById) + patientIdString
         serverRequestModel.disableSSLCertificateChecking()
         serverRequestModel.sendRequestToServer(queue, url,({ serverAnswer: String ->
             val patientID: Int = JSONObject(serverAnswer).getJSONObject("content").get("patientId") as Int
-            showResponse(patientID) }) )
+            returnResponse(patientID) }) )
     }
 
 
